8e4296503d54f44cf6bd36ae9b8fc2bbd21d3294
Elvis Ibarra <ielvisd@gmail.com>
2019-05-02 18:51:16 -0700

    reverse list

---
+++ Python/reverse_list.py
@@ -0,0 +1,11 @@
+def reverse_list(l):
+  'return a list with the reverse order of l'
+  l.reverse()
+  return l
+
+print(reverse_list([1,2,3,4])) # [4,3,2,1]
+print(reverse_list([3,1,5,4])) # [4,5,1,3]
+
+# Best Practice Solution
+def reverse_list(l):
+  return l[::-1]
