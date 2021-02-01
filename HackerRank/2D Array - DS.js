3d95dd72332a81cc2bff1c7fd3b782d1f4658ca8
ielvisd <ielvisd@gmail.com>
2021-01-31 16:41:07 -0800

    Add HackerRank - hourGlassSum - init

---
+++ HackerRank/2D Array - DS.js
@@ -0,0 +1,45 @@
+// Given a  2D Array, :
+
+// 1 1 1 0 0 0
+// 0 1 0 0 0 0
+// 1 1 1 0 0 0
+// 0 0 0 0 0 0
+// 0 0 0 0 0 0
+// 0 0 0 0 0 0
+
+// An hourglass in  is a subset of values with indices falling in this pattern in 's graphical representation:
+
+// a b c
+//   d
+// e f g
+
+// There are 16 hourglasses in arr. An hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every arr hourglass in , then print the maximum hourglass sum. The array will always be 6 x 6.
+
+// Sample Input
+
+// 1 1 1 0 0 0
+// 0 1 0 0 0 0
+// 1 1 1 0 0 0
+// 0 0 2 4 4 0
+// 0 0 0 2 0 0
+// 0 0 1 2 4 0
+
+// Sample Output
+
+// 19
+
+// Explanation
+
+// arr contains the following hourglasses:
+
+// The hourglass with the maximum sum (19) is:
+
+// 2 4 4
+//   2
+// 1 2 4
+
+// Complete the hourglassSum function below.
+
+function hourglassSum(arr) {}
+
+
